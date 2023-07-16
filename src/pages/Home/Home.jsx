2eddd33e5172f162a163285a0554bd7afcd85f3c
JannatunNimeNishat <nishatnime100@gmail.com>
2023-07-16 19:39:15 +0600

    bug fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h3>THis is home</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nam voluptates ad eveniet quo consectetur quia tenetur eos. Corporis ea animi qui facere suscipit eum adipisci quae dolore est recusandae!</p>
         </div>
     );
 };
